92047922962218e9782b1df9719c8a4a101795e6
Oscar Canales <hoscar161@gmail.com>
2025-05-10 23:28:07 -0600

    creacion de modelos y actulizacion de llamado de modelos en forms de autor y libros

---
+++ Proyecto2.Frontend/Models/AutorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Proyecto2.Frontend.Models;
+
+public class AutorDto
+{
+    public string NombreCompleto { get; set; } = "";
+    public DateTime? FechaNacimiento { get; set; }
+    public string Ciudad { get; set; } = "";
+    public string Correo { get; set; } = "";
+}
---
+++ Proyecto2.Frontend/Models/LibroDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto2.Frontend.Models;
+
+public class LibroDto
+{
+    public string Titulo { get; set; } = "";
+    public string Anio { get; set; } = "";
+    public string Genero { get; set; } = "";
+    public int? NumPaginas { get; set; }
+    public string Autor { get; set; } = "";
+}
