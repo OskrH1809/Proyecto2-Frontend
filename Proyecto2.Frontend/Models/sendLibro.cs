3d1297d76dbd645b6ae9bbb4d4597f00d214d20a
Oscar Canales <hoscar161@gmail.com>
2025-05-12 20:40:26 -0600

    ajuste en el formulario de edicion de libro

---
+++ Proyecto2.Frontend/Models/sendLibro.cs
@@ -0,0 +1,12 @@
+﻿namespace Proyecto2.Frontend.Models;
+
+public class SendLibro
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; } = "";
+    public int Anio { get; set; }
+    public string Genero { get; set; } = "";
+    public int? NumeroPaginas { get; set; }
+    public int AutorId { get; set; }
+
+}
